a17406946ee39663ed5e49d59329bf5a0184070c
Pedro Van Thienen <pedrovanthienen@gmail.com>
2020-09-09 19:41:20 -0400

    update

--- index.js
+++ index.js
@@ -40,15 +40,18 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(/*code here*/){
-
-    /*code here*/
-
-
-    ok 
+function is31Flavors(originalFlavors){
+    if (originalFlavors.length === 31){
+        return "TRUE";
+    }else {
+        return "FALSE";
+    }
 }
+console.log(is31Flavors(originalFlavors))
 
-/* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
+
+
+/* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! You need to modify the array to include this flavor. 
 
 Your function should accept:
 
@@ -59,14 +62,19 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
 
-    /*code here*/
 
+
+// to ask Chris: 'Rainbow Sherbert' is inserted wherever it says 'Flavor', why can't you just write 'array' at the begining of .unshift?
+
+function addFlavor(array, Flavor){
+    originalFlavors.unshift(Flavor);
+    console.log(originalFlavors);
 }
+addFlavor(originalFlavors,"Rainbow Sherbert")
 
 
-/* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
+/* Task 3: Your task is to remove an item from the end of the array. 
 
 Your function should accept:
 
@@ -76,11 +84,14 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
-
-    /*code here*/
-
+function removeLastFlavor(array){
+    array.pop();
+    console.log(originalFlavors)
 }
+removeLastFlavor(originalFlavors)
+
+
+
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
