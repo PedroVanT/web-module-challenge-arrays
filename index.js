504cb47ec742eb4f6589ca0bd34c50bcdda4d3cc
Pedro Van Thienen <pedrovanthienen@gmail.com>
2020-09-10 02:59:48 -0400

    almost done, on question 7

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ var originalFlavors = ["Banana Nut Fudge",
     "Vanilla",
     "Vanilla Burnt Almond"]
 
+
 /* Task 1: Confirm that the array is exactly 31 flavors. Your function should accept:
 
 (1) an array 
@@ -48,6 +49,8 @@ function is31Flavors(originalFlavors){
     }
 }
 console.log(is31Flavors(originalFlavors))
+
+
 
 
 
@@ -74,6 +77,9 @@ function addFlavor(array, Flavor){
 addFlavor(originalFlavors,"Rainbow Sherbert")
 
 
+
+
+
 /* Task 3: Your task is to remove an item from the end of the array. 
 
 Your function should accept:
@@ -93,6 +99,7 @@ removeLastFlavor(originalFlavors)
 
 
 
+
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
 Your function should accept:
@@ -102,11 +109,12 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(/*code here*/){
-
-    /*code here*/
-
+function getFlavorByIndex(array, index){
+    console.log(array[index]);
 }
+getFlavorByIndex(originalFlavors, 2)
+
+
 
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
@@ -121,14 +129,18 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/){
 
-    /*code here*/
-
+// josh did a loop, is that better? and why?
+function removeFlavorByName(array, flavor){
+    array.splice(array.indexOf(flavor),1);
+    console.log(originalFlavors)
 }
+removeFlavorByName(originalFlavors, "Vanilla")
 
 
-/* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
+
+
+/* Task 6: Write a function called copy that makes a copy of the array. 
 
 Your function should accept: 
 
@@ -136,13 +148,24 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
-
-    /*code here*/
-
+function copy(array){
+    let newArr = [...array]
+    return newArr;
 }
 
-/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
+let newArr = copy(originalFlavors)
+
+console.log(newArr)
+
+
+
+// console.log(originalFlavors) ------ this is just to see if the original array still works
+
+/* Task 7: create promotional materials highlighting all of their chocolate flavors. 
+
+Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. 
+
+Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
 Your function should accept: 
 
@@ -157,11 +180,20 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
+function filterByWord(array, flavor) {
 
-    /*code here*/
+    let newArr = [];
 
-}
+    for (let i = 0; i < array.length; i++) {
+
+            console.log(array[i]);
+
+        if (array[i].includes(flavor)) {
+    }
+    return original
+}    
+
+consule.log(filterByWord(originalFlavors, 'chocolate'))
 
 
 
